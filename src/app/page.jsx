37b125efc48b161bb3e6f464e56cc0e10387e3a8
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-06-17 21:07:56 +0530

    Made a promptopia hero-section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1 className="text-5xl tracking-tighter text-center">Hello <span className="text-orange-400">Promptopia</span>!</h1>
+export default function Home() {
+    return (
+        <section className="text-center">
+            <div className="sm:text-4xl w-full mt-5 text-2xl font-black">
+                <h1 className="">Discover & Share Prompts</h1>
+                <p className="text-orange-500 uppercase red_bule_gradient">AI Pompting Tool</p>
+            </div>
+
+            <div className="">
+              <p className="sm:text-lg sm:font-bold text-sm font-thin text-gray-600">An open source, AI prompt shareing tool.</p>
+            </div>
+        </section>
+    )
 }
