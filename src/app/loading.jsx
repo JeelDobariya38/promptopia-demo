a92f5da8ff8f30cef4af9aaa830ab9d2043e4be0
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-06-28 21:07:38 +0530

    Made email field free out of unique constraint

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,20 +1,3 @@
-// import NavigationBar from "@components/navigationbar";
-// import PostCard from "@components/postcard";
-
 export default function Loading() {
-  // let dummypost = {
-  //     "title": "Jeel Dobariya",
-  //     "prompt": "Radhe Radhe!!!",
-  //     "tags": "#radhe #krishna",
-  //     "createdAt": "2025-06-22T07:53:24.978Z"
-  // };
-
-  // return (
-  //     <section className="text-center">
-  //         <NavigationBar />
-  //         <PostCard post={dummypost} />
-  //     </section>
-  // )
-
   return <p>loading.....</p>;
 }
