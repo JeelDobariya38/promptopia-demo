5e6b12191149420ee3e0a990a5fd49e3a7c2940b
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-06-28 19:03:03 +0530

    Add utility function default string

--- src/lib/executeAction.js
+++ src/lib/executeAction.js
@@ -1,7 +1,7 @@
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 
-export default executeAction = async (actionFn, successMessage) => {
+export default async function executeAction(actionFn, successMessage = "Run successfully!!") {
     try {
         await actionFn();
 
