16f43455c1c171e8a4f87ede41cc967c3660d821
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-06-29 22:27:22 +0530

    Improve Logging

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,9 @@ import { verifySession } from '@lib/session';
 import { NextResponse } from 'next/server';
 
 function logRequest(method, requestType, pathname, middlewareOutcome) {
-    console.log(`[${method}] [${requestType}] ${pathname} -> [${middlewareOutcome}]`);
+    if (process.env.LOGGING) {
+        console.log(`[${method}] [${requestType}] ${pathname} -> [${middlewareOutcome}]`);
+    }
 }
 
 export async function middleware(request) {
@@ -12,18 +14,8 @@ export async function middleware(request) {
     const publicPaths = ["/"];
     const authPaths = ["/auth/signup", "/auth/login"]; // Paths where authentication is handled
 
-    let requestType = "UNKNOWN";
     if (publicPaths.includes(pathname)) {
-        requestType = "PUBLIC_PATH";
-    } else if (authPaths.includes(pathname)) {
-        requestType = "AUTH_PATH";
-    } else {
-        // Assume all other paths are protected by default
-        requestType = "PROTECTED_PATH";
-    }
-
-    if (publicPaths.includes(pathname)) {
-        logRequest(method, requestType, pathname, "ALLOWED");
+        logRequest(method, "PUBLIC_PATH", pathname, "ALLOWED");
         return NextResponse.next();
     }
 
@@ -31,21 +23,21 @@ export async function middleware(request) {
 
     // If authenticated and trying to access auth paths, redirect to home
     if (isAuth && authPaths.includes(pathname)) {
-        logRequest(method, requestType, pathname, "REDIRECTED_HOME");
+        logRequest(method, "AUTH_PATH", pathname, "REDIRECTED_HOME");
         const homeUrl = new URL('/', request.url);
         return NextResponse.redirect(homeUrl);
     }
 
     // If not authenticated and NOT on an auth path, redirect to login
     if (!isAuth && !authPaths.includes(pathname)) {
-        logRequest(method, requestType, pathname, "REDIRECTED_LOGIN");
+        logRequest(method, "PROTECTED_PATH", pathname, "REDIRECTED_LOGIN");
         const loginUrl = new URL('/auth/login', request.url);
         return NextResponse.redirect(loginUrl);
     }
 
     // For all other cases (e.g., authenticated user accessing protected paths,
     // or unauthenticated user on an auth path), allow the request to proceed.
-    logRequest(method, requestType, pathname, "ALLOWED");
+    logRequest(method, "PROTECTED_PATH", pathname, "ALLOWED");
     return NextResponse.next();
 }
 
