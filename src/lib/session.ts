32d6154f82a62ad7ab566d18205ed7a93ae17f8d
Jeel Dobariya <jeeldobariya15@gmail.com>
2025-07-10 13:14:31 +0530

    Migrate lib from js to ts

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,14 +1,31 @@
 import 'server-only';
-import { SignJWT, jwtVerify } from 'jose';
+import { SignJWT, jwtVerify, type JWTPayload } from 'jose';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
+type SessionPayload = JWTPayload & {
+    userId: string;
+    expiresAt: Date;
+  };
 
+export type VerifySessionResult = {
+    isAuth: true;
+    userId: string;
+  } | {
+    isAuth: false;
+  };
+
+
+// Ensure SESSION_SECRET is defined in your .env and accessible
 const secretKey = process.env.SESSION_SECRET;
+if (!secretKey) {
+  throw new Error('SESSION_SECRET environment variable is not set.');
+}
+
 const encodedKey = new TextEncoder().encode(secretKey);
 
 
-async function encrypt(payload) {
+async function encrypt(payload: SessionPayload) {
     return new SignJWT(payload)
         .setProtectedHeader({ alg: 'HS256' })
         .setIssuedAt()
@@ -16,18 +33,18 @@ async function encrypt(payload) {
         .sign(encodedKey);
 }
 
-async function decrypt(session) {
+async function decrypt(session: string): Promise<SessionPayload | undefined> {
     try {
         const { payload } = await jwtVerify(session, encodedKey, {
             algorithms: ['HS256'],
         });
-        return payload;
+        return payload as SessionPayload;
     } catch (error) {
         return undefined;
     }
 }
 
-export async function createSession(userId) {
+export async function createSession(userId: string): Promise<void> {
     const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
     const session = await encrypt({ userId, expiresAt });
     const cookieStore = await cookies();
@@ -41,23 +58,26 @@ export async function createSession(userId) {
     });
 }
 
-export const verifySession = async () => {
+export async function verifySession(): Promise<VerifySessionResult> {
     const cookie = (await cookies()).get('session')?.value;
-    const session = await decrypt(cookie);
 
-    if (session && session.userId) {
-        return { isAuth: true, userId: session.userId };
+    if (cookie) {
+        const session = await decrypt(cookie);
+
+        if (session && session.userId) {
+            return { isAuth: true, userId: session.userId };
+        }
     }
 
     return { isAuth: false };
 };
 
-export async function deleteSession() {
+export async function deleteSession(): Promise<void> {
     const cookieStore = await cookies();
     cookieStore.delete('session');
 }
 
-export const getUserID = async () => {
+export async function getUserID(): Promise<string> {
     let session = await verifySession();
 
     if (session.isAuth) {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,39 @@
-import { verifySession } from '@/lib/session';
-import { NextResponse } from 'next/server';
+import { verifySession, type VerifySessionResult } from '@/lib/session';
+import { NextResponse, NextRequest } from 'next/server';
 
-function logRequest(method, requestType, pathname, middlewareOutcome) {
+
+function logRequest(method: string, requestType: string, pathname: string, middlewareOutcome: string): void {
     if (process.env.LOGGING == "verbose") {
         console.log(`[${method}] [${requestType}] ${pathname} -> [${middlewareOutcome}]`);
     }
 }
 
-export async function middleware(request) {
-    const { pathname } = request.nextUrl;
-    const method = request.method;
 
-    const publicPaths = ["/", "/api/session"];
-    const authPaths = ["/auth/signup", "/auth/login"]; // Paths where authentication is handled
+export async function middleware(request: NextRequest): Promise<NextResponse> {
+    const { pathname } : { pathname: string } = request.nextUrl;
+    const method: string = request.method;
+
+    const publicPaths: string[] = ["/", "/api/session"];
+    const authPaths: string[] = ["/auth/signup", "/auth/login"]; // Paths where authentication is handled
 
     if (publicPaths.includes(pathname)) {
         logRequest(method, "PUBLIC_PATH", pathname, "ALLOWED");
         return NextResponse.next();
     }
 
-    const { isAuth } = await verifySession();
+    const { isAuth } : VerifySessionResult = await verifySession();
 
     // If authenticated and trying to access auth paths, redirect to home
     if (isAuth && authPaths.includes(pathname)) {
         logRequest(method, "AUTH_PATH", pathname, "REDIRECTED_HOME");
-        const homeUrl = new URL('/', request.url);
+        const homeUrl: URL = new URL('/', request.url);
         return NextResponse.redirect(homeUrl);
     }
 
     // If not authenticated and NOT on an auth path, redirect to login
     if (!isAuth && !authPaths.includes(pathname)) {
         logRequest(method, "PROTECTED_PATH", pathname, "REDIRECTED_LOGIN");
-        const loginUrl = new URL('/auth/login', request.url);
+        const loginUrl: URL = new URL('/auth/login', request.url);
         return NextResponse.redirect(loginUrl);
     }
 
@@ -40,6 +42,7 @@ export async function middleware(request) {
     logRequest(method, "PROTECTED_PATH", pathname, "ALLOWED");
     return NextResponse.next();
 }
+
 
 export const config = {
     // Match all paths except for static files (like /_next/static, /favicon.ico)
